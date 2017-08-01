6a795ec4121a42baf8e6eb079b0d5600663abf21
Daniel Villanueva <danielvillanueva94@gmail.com>
2017-08-01 08:50:17 +0200

    Changes: Completed "Getting started" section

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from  '@angular/core';
 
 @Component({
   selector: 'app-root',
-  styleUrls: ['app.component.scss'],
+  styleUrls:['app.component.scss'],
   template: `
     <div class="app">
-      Hello!
+      {{title}}
     </div>
   `
 })
 export class AppComponent {
-
+  title: string;
+  constructor() {
+    this.title = 'Ultimate Angular';
+  }
 }
